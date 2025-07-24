a22d210be41989dc127b005eb37001972c9ca0b0
om <123346380+omjamnekar@users.noreply.github.com>
2025-07-24 22:21:20 +0530

    add more files
    Co-Authored-By: ojamnekar <222563463+ojamnekar@users.noreply.github.com>

---
+++ fe.py
@@ -0,0 +1,20 @@
+print("challenge")
+
+for i in range(2,5):
+    print("challenge")
+
+print("challenge")
+
+for i in range(2,5):
+    print("challenge")
+
+print("challenge")
+
+for i in range(2,5):
+    print("challenge")
+
+
+print("challenge")
+
+for i in range(2,5):
+    print("challenge")
